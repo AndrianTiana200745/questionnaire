b209ebe9f8769d7a5a827336a8122337053cd178
AndrianTiana200745 <andrian7iana@gmail.com>
2025-07-30 15:53:39 +0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -174,4 +174,6 @@ def analyse():
         return jsonify({"error": f"Erreur interne serveur : {str(e)}"}), 500
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=5088, debug=True)
